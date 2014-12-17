238feb9f8774658ebf54707fa181be2e2d601c33
daya-prac <zonek163002@163.com>
2014-12-17 23:06:10 +0800

    backup V2.0

--- python/backup_v2.0.py
+++ python/backup_v2.0.py
@@ -4,8 +4,21 @@
 
 import os
 import time
-source = [r'D:\back', r'D:\other\Screenshots']
-target_dir = r'D:\code'
-today = time.strftime('%Y%m%d')
-target = target_dir + os.sep + today + time.strftime('%H%M%S') + '.zip'
+#source = [r'D:\back', r'D:\other\Screenshots']
+source = ['/home/zonek/test', '/home/zonek/projects']
+#target_dir = r'D:\code'
+target_dir = '/home/zonek/code/'
+today = target_dir + time.strftime('%Y%m%d')
+now = time.strftime('%H%M%S')
+target = today + os.sep + now + '.zip'
 
+if not os.path.exists(today):
+    os.mkdir(today)
+    print '创建了目录：', today
+
+zip_command = 'zip -qr %s %s' % (target, ' '.join(source))
+
+if os.system(zip_command) == 0:
+    print '备份成功：', target
+else:
+    print '备份失败'
