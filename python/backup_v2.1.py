a0642fadc7494800c55af39373eaa6a1f2f5d0d2
daya-prac <zonek163002@163.com>
2014-12-19 00:44:08 +0800

    backup V2.1

---
+++ python/backup_v2.1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding: utf-8
+#Filename: backup_v2.1
+
+import os
+import time
+
+source = ['/home/zonek/test', '/home/zonek/projects']
+target_dir = '/home/zonek/code'
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+now = time.strftime('%H%M%S')
+#target = today + os.sep + now + '.zip'
+
+if not os.path.exists(today):
+    os.mkdir(today)
+
+#zip_command = 'zip -qr %s %s' % (target, " ".join(source))
+
+comment = raw_input('请输入：')
+if len(comment) == 0:
+    target = today + os.sep + now + '.zip'
+    zip_command = 'zip -qr %s %s' % (target, " ".join(source))
+    if os.system(zip_command) == 0:
+        print '备份成功：',target
+    else:
+        print '备份失败'
+else:
+    target = today + os.sep + now + '_' + comment + '.zip'
+    zip_command = 'zip -qr %s %s' % (target, " ".join(source))
+    if os.system(zip_command) == 0:
+        print '备份成功：',target
+    else:
+        print '备份失败'
